e6cffae2f096d86520d26f76f8bbe4c193bb5230
Jose Lopez Cova Martin <jlopmar0912@g.educaand.es>
2025-10-15 12:34:15 +0200

    enunciado del ejercicio

--- 3.2/src/Main.java
+++ 3.2/src/Main.java
@@ -3,6 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+
+        /* Implementar una aplicación para calcular datos estadísticos de las edades de los alumnos de un centro educativo.
+        SE introducirán datos hasta que uno de ellos sea negativo, y se mostrará: la suma de todas las edades introducidas,
+        la media, el número de alumnos y cuántos son mayores de edad. (pag 74)*/
+
+
+
+
         //variable
         int iEdad = 0;
         int iMayorEdad = 0;
