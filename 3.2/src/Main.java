c652678d90425baa3bb7c5c9fd55e5f4e0b32f6f
Jose Lopez Cova Martin <jlopmar0912@g.educaand.es>
2025-10-15 12:30:37 +0200

    no entiendo este ejercicio todavia

--- 3.2/src/Main.java
+++ 3.2/src/Main.java
@@ -1,55 +1,29 @@
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-
-        /* Implementar una aplicación para calcular datos estadísticos de las edades de los alumnos de un centro educativo.
-SE introducirán datos hasta que uno de ellos sea negativo, y se mostrará: la suma de todas las edades introducidas,
-la media, el número de alumnos y cuántos son mayores de edad. (pag 74)*/
-
-
-        //Entrada
-        System.out.println("Introduce las edades de los alumnos: ");
-
-        //Persistencia
-        double dEdad;
-        int iMayorEdad;
+        //variable
+        int iEdad = 0;
+        int iMayorEdad = 0;
         int iSumaEdades;
-        int iNumeroAlumnos = 0;
-
-
-        //Entrada
-        Scanner sc = new Scanner(System.in);
-
-        //Persistencia que es introducida mediante teclao'
-        dEdad = sc.nextDouble();
-        iMayorEdad = sc.nextInt();
-        iSumaEdades = sc.nextInt();
-
+        int iNumAlumnos = 0;
 
         do {
-            System.out.println("Primer alumno: ");
-            //Si es mayor de 18 se guarda en el contador mayor de edad.
-            iNumeroAlumnos++;
-            if (dEdad >= 18) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Introduce la edad del alumno: ");
+            if (iEdad > 18) {
                 iMayorEdad++;
             }
-            //Actualizar el bucle
-            dEdad = sc.nextInt();
+            iNumAlumnos++;
+            iEdad = sc.nextInt();
+        } while (iEdad > 0);
+            iSumaEdades = iEdad + iEdad;
 
-        } while (dEdad > 0); //Condicion del bucle para que sea verdad.
 
-        System.out.println("Numero de mayores de edad: " + iMayorEdad);
         System.out.println("Suma de edades: " + iSumaEdades);
-        System.out.println("Numero de alumnos: " + iNumeroAlumnos);
-        System.out.println("Media de edades: " + iSumaEdades/iNumeroAlumnos); /*redondea la media y me da igual la
-                                                                                verdad paso de cambiar el tipo de dato*/
-
-
-
-
+        System.out.println("Media de edad: "+ (iSumaEdades/iNumAlumnos));
+        System.out.println("Mayores de edad: " + iMayorEdad);
+        System.out.println("Numero de alumnos: " + iNumAlumnos);
     }
 }
